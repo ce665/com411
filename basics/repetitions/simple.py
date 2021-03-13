e283c95e665ea9180fa8a7bd4e06c96554fcf914
repl.it user <replituser@example.com>
2021-03-13 17:51:17 +0000

    Added code to demonstrate the simple use of while looop

---
+++ basics/repetitions/simple.py
@@ -0,0 +1,11 @@
+print("How many cables i should remove?")
+
+cables =int(input())
+
+cable_removed= 0
+ 
+print()
+while ( cable_removed < cables):
+  print("removed cable")
+  cable_removed=cable_removed + 1
+ 
