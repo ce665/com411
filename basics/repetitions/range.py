57896c1ebe98cb8fb0f7f6ee7050ae7f9f90b25d
repl.it user <replituser@example.com>
2021-03-20 15:47:28 +0000

    added code to demonstrtate tehsimple use of for loop

---
+++ basics/repetitions/range.py
@@ -0,0 +1,8 @@
+print("what level of brightness is required")
+even=int(input())
+
+print()
+
+for count in range (2,even+1,2) :
+  print("bep brightness:","*"*count)
+  print("Bop brightness:","*"*count)
