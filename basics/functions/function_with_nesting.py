5a40fd5656dba35d7321d3567b4602683dcdbc1f
repl.it user <replituser@example.com>
2021-04-08 11:58:14 +0000

    Added code to demonstrate the def function with nested decision

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,10 @@
+def identify():
+  print("What lies before us")
+  response=input() 
+  if (response== "large bouldier"):
+    print("Its time to run ")
+
+  else :
+   print("We will be fine")
+
+identify() 
